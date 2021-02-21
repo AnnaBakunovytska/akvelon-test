b5fc58de862b0b25175a22fa382883f039430b80
Anna B <annabakunovytska@gmail.com>
2021-02-22 00:16:01 +0200

    fix components

---
+++ src/components/invoices.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export const Title = () => <h2>Invoices</h2>;
+export const ButtonAddNew = () => {
+  return (
+    <div>
+      <h3>Invoices</h3>
+      <button className='add_button'>Add new</button>
+    </div>
+  );
+};
+export const Invoices = () => <h3>Invoices</h3>
+
